f9bc4d085cd558ff6dfbeb3ad9e529a494e4e1f2
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-21 07:37:47 +0600

    hero slider

--- my-project/src/Components/Featured.jsx
+++ my-project/src/Components/Featured.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
 
 const Featured = () => {
+  let [apidata, setApiData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+    .then(res => res.json())
+    .then(data => setApiData(data.products))
+
+  },[])
     
   return (
     <>
       <div className="mt-10 ">
         <h1 className='text-center text-2xl font-bold font-josefin'>Featured Products</h1>
-        <div className="App">
+        <div className="flex flex-wrap gap-5 items-center justify-around">
+          {apidata.map((item)=>(
+           <img className='border w-[250px]' src={item.thumbnail} alt="" />
+          ))}
+
 
         </div>
 
--- my-project/src/Components/Hero.jsx
+++ my-project/src/Components/Hero.jsx
@@ -1,27 +1,98 @@
 import React from 'react'
 import Lamp from '../assets/Lamp.png'
 import All from '../assets/all.png'
+import Slider from "react-slick";
 const Hero = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <>
-      <div className=" md:flex md:px-10 md:pb-20   bg-[#f2eff2] md:justify-between ">
+    <section>
+      
+        <Slider {...settings}>
+          
+          <div className="">
+            <div className=" md:flex md:px-10 md:pb-20   bg-[#f2eff2] md:justify-between ">
+              <div className="">
+                  <img src={Lamp} alt="" />
+                  
+              </div>
+
+              <div className="md:mt-44 relative md:right-16 ">
+                  <p className='text-primary'>Best Furniture For Your Castle....</p>
+                  <h1 className='md:text-4xl font-josefin text-xl mt-4'>New Furniture Collection <br />Trends in 2020</h1>
+                  <p className='mt-2 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est  dipiscing in phasellus non in justo.</p>
+                  <button className='border text-white bg-biguni px-5 py-2 text-[13px] mt-2'>Shop Now</button>
+              </div>
+
+              <div className="w-[40%] md:mt-20 relative md:right-10">
+                  <img src={All} alt="" />          
+
+              </div>
+          </div>
+          </div>
+          
+          <div className="">
+            <div className=" md:flex md:px-10 md:pb-20   bg-[#f2eff2] md:justify-between ">
+            <div className="">
+                <img src={Lamp} alt="" />
+                
+            </div>
+
+            <div className="md:mt-44 relative md:right-16 ">
+                <p className='text-primary'>Best Furniture For Your Castle....</p>
+                <h1 className='md:text-4xl font-josefin text-xl mt-4'>New Furniture Collection <br />Trends in 2020</h1>
+                <p className='mt-2 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est  dipiscing in phasellus non in justo.</p>
+                <button className='border text-white bg-biguni px-5 py-2 text-[13px] mt-2'>Shop Now</button>
+            </div>
+
+            <div className="w-[40%] md:mt-20 relative md:right-10">
+                <img src={All} alt="" />          
+
+            </div>
+          </div>
+          </div>
+          
         <div className="">
-            <img src={Lamp} alt="" />
-            
-        </div>
+            <div className=" md:flex md:px-10 md:pb-20   bg-[#f2eff2] md:justify-between ">
+            <div className="">
+                <img src={Lamp} alt="" />
+                
+            </div>
 
-        <div className="md:mt-44 relative md:right-16 ">
-            <p className='text-primary'>Best Furniture For Your Castle....</p>
-            <h1 className='md:text-4xl font-josefin text-xl mt-4'>New Furniture Collection <br />Trends in 2020</h1>
-            <p className='mt-2 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est  dipiscing in phasellus non in justo.</p>
-            <button className='border text-white bg-biguni px-5 py-2 text-[13px] mt-2'>Shop Now</button>
-        </div>
+            <div className="md:mt-44 relative md:right-16 ">
+                <p className='text-primary'>Best Furniture For Your Castle....</p>
+                <h1 className='md:text-4xl font-josefin text-xl mt-4'>New Furniture Collection <br />Trends in 2020</h1>
+                <p className='mt-2 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna in est  dipiscing in phasellus non in justo.</p>
+                <button className='border text-white bg-biguni px-5 py-2 text-[13px] mt-2'>Shop Now</button>
+            </div>
 
-        <div className="w-[40%] md:mt-20 relative md:right-10">
-            <img src={All} alt="" />          
+            <div className="w-[40%] md:mt-20 relative md:right-10">
+                <img src={All} alt="" />          
 
-        </div>
-      </div>
+            </div>
+          </div>
+          </div>
+        
+          
+          
+        </Slider>
+   
+
+    </section>
+
+  
+
+      
+
+
+
+
     </>
   )
 }
