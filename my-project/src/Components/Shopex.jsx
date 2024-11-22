73ccfb2ef1d7ae2733c8a09cbdb7dfb93c09ae80
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-22 12:34:41 +0600

    shadow

--- my-project/src/Components/Shopex.jsx
+++ my-project/src/Components/Shopex.jsx
@@ -11,27 +11,27 @@ const Shopex = () => {
     <div className="">
         <h2 className='text-2xl font-bold mt-10 font-josefin text-center '>What Shopex Offer!</h2>
 
-      <div className="container mx-auto flex   justify-between mt-14 pb-10">
+      <div className="container md:mx-auto md:flex justify-items-center    md:col-span-4  md:justify-between mt-8 md:mt-14 pb-10">
 
-        <div className="border w-[220px] text-center   px-3 py-8">
+        <div className=" w-[220px] text-center  px-3 py-8 shadow-md   ">
             <img className=' mx-auto ' src={Car} alt="" />
             <h2 className='text-2xl font-bold mt-5'>24/7 Support</h2>
             <p className='mt-5 text-[12px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.</p>
         </div>
 
-        <div className="border w-[220px]  text-center   px-3 py-8">
+        <div className=" w-[220px]  text-center   px-3 py-8 shadow-md">
             <img className=' mx-auto ' src={Dollar} alt="" />
             <h2 className='text-2xl font-bold mt-5'>24/7 Support</h2>
             <p className='mt-5 text-[12px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.</p>
         </div>
 
-        <div className="border w-[220px]  text-center   px-3 py-8">
+        <div className=" w-[220px]  text-center   px-3 py-8 shadow-md">
             <img className=' mx-auto ' src={Logo} alt="" />
             <h2 className='text-2xl font-bold mt-5'>24/7 Support</h2>
             <p className='mt-5 text-[12px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.</p>
         </div>
 
-        <div className="border w-[220px] text-center    px-3 py-8">
+        <div className=" w-[220px] text-center    px-3 py-8 shadow-md">
             <img className=' mx-auto ' src={Contact} alt="" />
             <h2 className='text-2xl font-bold mt-5'>24/7 Support</h2>
             <p className='mt-5 text-[12px] '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa purus gravida.</p>
