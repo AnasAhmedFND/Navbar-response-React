2121234a6d4203765e9c534451e74f59517cfe7e
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-23 20:40:59 +0600

    product blog

--- my-project/src/Components/Navbar.jsx
+++ my-project/src/Components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
         </div>
 
         <div className="md:flex md:justify-between  w-[80%] ">
-            <ul className={`md:flex md:gap-5 font-lato items-center  md:static  ${menuShow ? 'absolute top-20 left-36 pb-40 ' : 'absolute top-0 -left-72' }`}>
+            <ul className={`md:flex md:gap-5 font-lato items-center   md:static  ${menuShow ? 'absolute top-20 left-34 pb-40 ' : 'absolute top-0 -left-72' }`}>
                 <li  className='hover:text-primary'><Link to='/'>Home</Link></li>
                 <li className='hover:text-primary'><Link to='/shop'>Pages</Link> </li>
                 <li className='hover:text-primary'>Products</li>
