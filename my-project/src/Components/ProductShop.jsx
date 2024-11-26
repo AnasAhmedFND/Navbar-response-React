8f3ca4419064c30857cbf1b0f43d8364bb7dcd7d
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-26 17:17:05 +0600

    category select

--- my-project/src/Components/ProductShop.jsx
+++ my-project/src/Components/ProductShop.jsx
@@ -1,8 +1,18 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { apiData } from './ContextApi'
+import { TiArrowSortedDown } from "react-icons/ti";
 
 const ProductShop = () => {
     let shopApiProduct = useContext(apiData)
+    
+   let [category, setCategory] = useState([])
+
+   useEffect(() => {
+        setCategory([...new Set(shopApiProduct.map((item)=> item.category))])
+   }, [shopApiProduct])
+   console.log(category);
+   
+    
     
     
   return (
@@ -10,12 +20,14 @@ const ProductShop = () => {
       <div className="container mx-auto flex justify-between py-10">
 
         <div className="w-[18%] ">
-            <ul>
-                <li>categories 1</li>
-                <li>categories 2</li>
-                <li>categories 3</li>
-                <li>categories 4</li>
-                <li>categories 5</li>
+            <div className="">
+                 <h1 className='flex items-center font-bold'>Shop By Category <span className='ml-5'><TiArrowSortedDown /></span></h1>
+                 
+            </div>
+            <ul className='overflow-y-scroll h-36'>
+                  {category.map((item) =>(
+                    <li>{item}</li>
+                  ))}          
             </ul>
         </div>
 
@@ -25,7 +37,7 @@ const ProductShop = () => {
                  <img className='border bg-[#f5f5f8] ' src={item.thumbnail} alt="" />
                  <h4 className='font-bold'>{item.title} </h4>
                  <p className='relative bottom-20'><span className='text-[80px] text-[#f227a1] '>.</span><span className='text-[80px] text-[#f53044] '>.</span><span className='text-[80px] text-[#3330eb] '>.</span></p>
-                 <p className='relative bottom-24'>{item.price} <span className='text-primary '>{item.discountPercentage}</span> </p>
+                 <p className='relative bottom-24'>{item.price} <span className='text-primary ml-2'>{item.discountPercentage}</span> </p>
                </div>
             ))
                 
