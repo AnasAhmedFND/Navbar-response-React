00fff7663c52d5adec9668083d36279f4056558a
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-26 19:27:42 +0600

    category filter do item

--- my-project/src/Components/ProductShop.jsx
+++ my-project/src/Components/ProductShop.jsx
@@ -10,10 +10,17 @@ const ProductShop = () => {
    useEffect(() => {
         setCategory([...new Set(shopApiProduct.map((item)=> item.category))])
    }, [shopApiProduct])
-   console.log(category);
-   
-    
-    
+  
+   let [categoryShow, setCategoryShow] = useState(false)
+
+     let [categoryItem, setCategoryItem] = useState([])
+    const hendleCategory = (c) => {
+        let filteredProduct = shopApiProduct.filter((item) => item.category == c)
+        setCategoryItem(filteredProduct)
+
+        console.log(categoryItem);
+        
+    }
     
   return (
     <>
@@ -21,19 +28,23 @@ const ProductShop = () => {
 
         <div className="w-[18%] ">
             <div className="">
-                 <h1 className='flex items-center font-bold'>Shop By Category <span className='ml-5'><TiArrowSortedDown /></span></h1>
+                 <h1 onClick={() => setCategoryShow(!categoryShow)} className='flex items-center font-bold'>Shop By Category <span className='ml-5'><TiArrowSortedDown /></span></h1>
                  
             </div>
-            <ul className='overflow-y-scroll h-36'>
-                  {category.map((item) =>(
-                    <li>{item}</li>
-                  ))}          
-            </ul>
+            {categoryShow &&
+                <ul className='overflow-y-scroll h-36'>
+                {category.map((item) =>(
+                  <li onClick={() => hendleCategory (item)}>{item}</li>
+                ))}          
+          </ul>
+            }
         </div>
 
         <div className="w-[80%] flex flex-wrap gap-4 overflow-y-scroll h-[500px] ">
-            {shopApiProduct.map((item) => (
-               <div className="border w-[250px] h-[363px] text-center  ">
+
+            {categoryItem.length == 0 ? 
+            shopApiProduct.map((item) => (
+               <div className="border w-[250px] h-[363px] text-center shadow-xl ">
                  <img className='border bg-[#f5f5f8] ' src={item.thumbnail} alt="" />
                  <h4 className='font-bold'>{item.title} </h4>
                  <p className='relative bottom-20'><span className='text-[80px] text-[#f227a1] '>.</span><span className='text-[80px] text-[#f53044] '>.</span><span className='text-[80px] text-[#3330eb] '>.</span></p>
@@ -41,7 +52,23 @@ const ProductShop = () => {
                </div>
             ))
                 
+            
+
+            :
+
+            categoryItem.map((item) => (
+                <div className="border w-[250px] h-[363px] text-center shadow-xl ">
+                  <img className='border bg-[#f5f5f8] ' src={item.thumbnail} alt="" />
+                  <h4 className='font-bold'>{item.title} </h4>
+                  <p className='relative bottom-20'><span className='text-[80px] text-[#f227a1] '>.</span><span className='text-[80px] text-[#f53044] '>.</span><span className='text-[80px] text-[#3330eb] '>.</span></p>
+                  <p className='relative bottom-24'>{item.price} <span className='text-primary ml-2'>{item.discountPercentage}</span> </p>
+                </div>
+             ))
+                 
             }
+            
+
+
 
         </div>
 
