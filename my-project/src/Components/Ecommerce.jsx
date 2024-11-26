02955322d9a0a404b2e1069dd96960a08cac510e
AnasAhmedFND <anasahmed4402@gmail.com>
2024-11-26 19:30:46 +0600

    text size

--- my-project/src/Components/Ecommerce.jsx
+++ my-project/src/Components/Ecommerce.jsx
@@ -9,17 +9,17 @@ const Ecommerce = () => {
       <div className="container mx-auto">
        <div className="flex py-20">
        <div className="">
-            <h3 className='font-bold'>Ecommerce Acceories & Fashion item </h3>
+            <h3 className='font-bold text-primary '>Ecommerce Acceories & Fashion item </h3>
             <p className='text-[12px] text-[#8c8ca4] '>About 9,620 results (0.62 seconds)</p>
         </div>
 
         <div className="flex items-center ml-20">
-            <p>Per Page:</p>
+            <p className='text-[12px] '>Per Page:</p>
             <input className='border border-black w-[50px] ml-2' type="text" />
         </div>
 
         <div className="flex items-center ml-10">
-            <p>Sort By:</p>
+            <p className='text-[12px] '>Sort By:</p>
             <div className="border border-black w-[120px] flex items-center px-2 ml-2 ">
             <input className='w-[80%] ' type="text" placeholder='Best Match'  />
             <p className='w-[10%] text-[#b6b6ce] '> <IoIosArrowDown /></p>
